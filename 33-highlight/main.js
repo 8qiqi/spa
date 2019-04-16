0cd2f08f605366ec13d9f4b4ce34704c50a390db
8qiqi <3510363360@qq.com>
2019-04-16 12:47:41 +0800

    33-highlight

--- 33-highlight/main.js
+++ 33-highlight/main.js
@@ -1,22 +1,17 @@
-$(function() {
-    var e = new Behave({
-        textarea: $(".main textarea").get(0),
-        replaceTab: !0,
-        softTabs: !0,
-        tabSize: 2,
-        autoOpen: !0,
-        overwrite: !0,
-        autoStrip: !0,
-        autoIndent: !0,
-        fence: !1
-    });
-    return $('input[type="button"]').click(function() {
-        var e = $('<div><pre class="javascript"></pre></div>')
-          , t = $(".main textarea");
-        "" !== t.val() && (e.find("pre").html(t.val()),
-        hljs.highlightBlock(e.find("pre").get(0)),
-        $(".main").append(e))
-    }),
-    e
-});
+$(function(){
+  var $btnAddCode=$('input[type="button"]'); 
+  
+  $btnAddCode.click(function(){
+    var $code=$('<div><pre class="javascript"></pre><button>删除</button></div>');
 
+    $code.find('pre').html($('.code').val());
+    $('div.main').append($code);
+    hljs.highlightBlock($code.find('pre').get(0));
+
+    var $btn=$code.find('button');
+       
+    $btn.click(function(){
+      $btn.parent().remove();
+    });
+  });
+});
