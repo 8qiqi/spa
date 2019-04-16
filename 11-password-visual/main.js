9f244e79a88d59bbf5601796d189d600d59e0b23
8qiqi <3510363360@qq.com>
2019-04-16 13:07:58 +0800

    11-password-visual

--- 11-password-visual/main.js
+++ 11-password-visual/main.js
@@ -1,13 +1,17 @@
 $(function(){
-  var n=$("#pwd"),o=$(".pwd-text"),e=$("#eye");
-  n.on("input",function(){
-    o.val(n.val())
-  }),
-  e.mouseover(function(){
-    o.css("z-index","10")
-  }),
-  e.mouseout(function(){
-    o.css("z-index","-10")
-  })
-});
+  var $pwd=$('#pwd'),
+      $pwdText=$('.pwd-text'),
+      $eye=$('#eye');
+  $pwd.on('input',function(){
+    $pwdText.val($pwd.val());
+  });
 
+  $eye.mouseover(function(){
+    $pwdText.css('z-index','10');
+  });
+
+  $eye.mouseout(function(){
+    $pwdText.css('z-index','-10');
+  });
+  
+});
