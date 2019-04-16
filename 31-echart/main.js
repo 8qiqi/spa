7c8845af2c55577e96f4360986b8097986b6c733
8qiqi <3510363360@qq.com>
2019-04-16 12:38:11 +0800

    31-echart

--- 31-echart/main.js
+++ 31-echart/main.js
@@ -1,5 +1,5 @@
 /*global echarts: true*/
-$function() {
+$(function() {
     for (var t, a = [], e = [], n = 0; n <= 1; n += .1)
         a.push(o(n, 1)),
         e.push(o(-1 * (r(t = n) + r(1 - t)), 2));
